5b8f10358407f9257b4b97ea2078573fe6767db7
jpablolima <jp.pablolima@gmail.com>
2020-09-28 12:07:35 -0300

    initial methods

--- src/server.js
+++ src/server.js
@@ -1,3 +1,17 @@
+const proffs = [{
+    name: "João Pablo",
+    avatar: "https://instagram.fbsb15-1.fna.fbcdn.net/v/t51.2885-19/s150x150/119713586_180415873580102_2347263711263243162_n.jpg?_nc_ht=instagram.fbsb15-1.fna.fbcdn.net&_nc_ohc=qrPdJCWc-4wAX9xZoh0&oh=fcfdd0ae0352b303e56283e61da5329e&oe=5F93CD80",
+    whatsap: 6199807353,
+    bio: "Entusiasta e apaixonado por tecnologias.<br><br>Software Developer | Pós-Graduado Banco de Dados e Business Intelligence Soft Skills: ReactJs, NodeJs, JavaScript, HTML, CSS, Python, Git, Criação de API, Power BI, MongoDB, SQL, Heroku,Linux",
+    subject: "Programação",
+    cost: 50,
+    weekday: [0],
+    time_frm: [720],
+    time_to: [1220]
+}]
+
+
+
 const express = require('express');
 const server = express();
 
